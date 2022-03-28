0587456d4e9c2598c04587e136237bbbd18bd50b
yangcaifeng <2158586446@qq.com>
2022-03-28 10:28:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yangcaifeng
+ * @create 2022-03-28 10:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
